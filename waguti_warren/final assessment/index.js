6131fc62627852ec9829b6328163cb823dc31404
Waguti Warren <warrenw639@gmail.com>
2023-04-28 12:22:35 +0300

    created form

--- waguti_warren/final assessment/index.js
+++ waguti_warren/final assessment/index.js
@@ -6,6 +6,10 @@ const router = express.Router();
 
 
 
+app.set("view engine", "pug");
+app.set("views",path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 
 
---
+++ waguti_warren/final assessment/validate.js
@@ -0,0 +1,24 @@
+const validate = (form) => {
+    let error = 0;
+    //input
+
+
+    //error
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    if (error > 0) {
+        form.preventDefault();
+    }
+}
