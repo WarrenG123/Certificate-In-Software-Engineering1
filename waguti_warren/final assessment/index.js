fed4483a552dd38979f5ba94dc7fee8c19fdad37
Waguti Warren <warrenw639@gmail.com>
2023-04-28 10:52:31 +0300

    start of project

---
+++ waguti_warren/final assessment/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => console.log('listening on port 3000'));   
